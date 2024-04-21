3d716c7afb339e8ee345ff05d3453dda17d5e386
Ali Dulacha <alidulacha@Mac.local>
2024-04-21 11:34:54 +0300

    added home but in php format

---
+++ vacation.php
@@ -0,0 +1,23 @@
+<?php
+include "head.php"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vacation</title>
+	<link rel="stylesheet" type="text/css" href="work.css">
+</head>
+<body>
+	<div class="content">
+		<h1>EMBRACE YOUR DREAM VACATION</h1>
+        <div class="cell">
+            <a href="scuba.html">Scuba diving</a>
+            <a href="surfing.html">Kite surfing</a>
+            <a href="cycling.html">Cycling</a>
+        </div>
+		</div>
+</div>
+</body>
+</html>
