c8a7004d498c6e0f4d1122c1d73d3d4876a53705
Ali Dulacha <alidulacha@Mac.local>
2024-04-21 11:26:33 +0300

    added header

---
+++ head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vacation</title>
+	<link rel="stylesheet" type="text/css" href="work.css">
+</head>
+<body>
+	<div class="banner">
+		<div class="navbar">
+			<img src="573-5737498_travel-continuously-png-logo-black-silhouette-transparent-png-removebg-preview.png" class="logo">
+			<ul>
+				<li><a href="vacation.php">Home</a></li>
+				<li><a href="scuba.php">Scuba Diving</a></li>
+				<li><a href="surfing.php">Kite surfing</a></li>
+				<li><a href="cycling.php">Cycling</a></li>
+				<li><a href="index.php">Register</a></li>
+			</ul>
+	</div>
